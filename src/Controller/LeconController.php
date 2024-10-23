01947929f56058b2b366e20eca5febe6d609e061
Asma <saidiasma422@gmail.com>
2024-10-23 22:49:41 +0200

    📦 NEW: Add the app_lecon in the leconController and the view that come with it

--- src/Controller/LeconController.php
+++ src/Controller/LeconController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Lecon;
+use App\Repository\LeconRepository;
+use App\Repository\MatiereRepository;
+use App\Repository\ExerciceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LeconController extends AbstractController
 {
+
+
     #[Route('/lecon', name: 'app_lecon')]
-    public function index(): Response
+    public function index(LeconRepository $leconRepository,MatiereRepository $matiereRepository): Response
     {
+        $matieres = $matiereRepository->findBy([],["nom" => "ASC"]);
+
+        $lecons = $leconRepository->findBy([],["titre" => "ASC"]);
         return $this->render('lecon/index.html.twig', [
-            'controller_name' => 'LeconController',
+            'lecon' => 'lecon',
+            'lecons' => $lecons,
+            'matieres' => $matieres,
+
+        ]);
+    }
+
+    #[Route('/lecon/{id}', name: 'show_lecon')]
+    public function show(Lecon $lecon,ExerciceRepository $exerciceRepository,MatiereRepository $matiereRepository): Response
+    {
+        $matieres = $matiereRepository->findBy([],["nom" => "ASC"]);
+        // Récupérer les exercices associés à cette leçon
+        $exercices = $exerciceRepository->findBy([],["titre" => "ASC"]);
+        return $this->render('lecon/show.html.twig', [
+            'lecon' => $lecon,
+            'exercices' => $exercices,
+            'matieres' => $matieres,
+
         ]);
     }
 }
