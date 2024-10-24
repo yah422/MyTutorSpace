379cc9b9bf0f2a86ccc410a9150b71cdd1ac0e41
Asma <saidiasma422@gmail.com>
2024-10-24 09:14:07 +0200

    📦 NEW: Add the function addLecon to the LeconController nad cretae the view to display it

--- src/Controller/LeconController.php
+++ src/Controller/LeconController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Lecon;
+use App\Form\LeconType;
 use App\Repository\LeconRepository;
 use App\Repository\MatiereRepository;
 use App\Repository\ExerciceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LeconController extends AbstractController
@@ -26,6 +30,68 @@ class LeconController extends AbstractController
             'matieres' => $matieres,
 
         ]);
+    }
+
+    
+    // Méthode pour ajouter une Leçon
+    #[Route('/lecon/ajouter', name: 'add_lecon')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function add(MatiereRepository $matiereRepository,Lecon $lecon, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $matieres = $matiereRepository->findBy([],["nom" => "ASC"]);
+
+        // Création d'une nouvelle instance de Matiere
+        $categorie = new lecon();
+
+        // Création du formulaire
+        $form = $this->createForm(LeconType::class, $lecon);
+
+        // Gestion de la soumission du formulaire
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarde de la matière en base de données
+            $entityManager->persist($lecon);
+            $entityManager->flush();
+
+            // Message de confirmation
+            $this->addFlash('success', 'Leçon ajoutée avec succès !');
+
+            // Redirection vers la page de liste des leçons (à ajuster si nécessaire)
+            return $this->redirectToRoute('app_lecon');
+        }
+
+        // Rendu de la vue avec le formulaire
+        return $this->render('lecon/add.html.twig', [
+            'form' => $form->createView(),
+            'matieres' => $matieres,
+
+        ]);
+    
+    }
+
+    #[Route('/lecon/supprimer/{id}', name: 'delete_lecon', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(Request $request, CsrfTokenManagerInterface $csrfTokenManager, lecon $lecon, EntityManagerInterface $entityManager,int $id): Response
+    {
+        if (!$lecon) {
+            throw $this->createNotFoundException('No leçon found for id ' . $id);
+        }
+     
+        // Vérifier le token CSRF
+        if ($this->isCsrfTokenValid('delete_lecon', $request->request->get('_token'))) {
+            // Si valide, on peut supprimer la categorie
+            $entityManager->remove($lecon);
+            $entityManager->flush();
+
+            // Redirection après suppression
+            return $this->redirectToRoute('app_lecon');
+        }
+     
+        // Si le token est invalide, lever une exception
+        throw $this->createAccessDeniedException('Token CSRF invalide.');
+        
     }
 
     #[Route('/lecon/{id}', name: 'show_lecon')]
---
+++ src/Form/LeconType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lecon;
+use App\Entity\Matiere;
+use App\Entity\Niveau;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LeconType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('dateCreation', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('matiere', EntityType::class, [
+                'class' => Matiere::class,
+                'choice_label' => 'id',
+            ])
+            ->add('niveau', EntityType::class, [
+                'class' => Niveau::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('users', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lecon::class,
+        ]);
+    }
+}
