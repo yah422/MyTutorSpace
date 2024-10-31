1dce8a1594fad1e37f59a2af5b6f58378c114bdc
AsmaS <saidiasma422@gmail.com>
2024-10-31 11:18:01 +0100

    🐛 FIX: Removed the differents doublon file and cretae another show.html.twig and correct the UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,49 +23,43 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/user/admin/{id}', name: 'app_user_admin')]
+    
+
+    // Méthode pour ajouter une Ressource
+    #[Route('/user/ajouter', name: 'add_user')]
     #[IsGranted('ROLE_ADMIN')]
-    public function adminProfile(User $user, MatiereRepository $matiereRepository): Response
+    public function add(MatiereRepository $matiereRepository, User $user, Request $request, EntityManagerInterface $entityManager): Response
     {
         $matieres = $matiereRepository->findBy([], ["nom" => "ASC"]);
-        return $this->render('user/admin.html.twig', [
-            'user' => $user,
-            'matieres' => $matieres,
-        ]);
-    }
+        // Création d'une nouvelle instance de User
+        $user = new User();
 
-    #[Route('/user/parent/{id}', name: 'app_user_parent')]
-    #[IsGranted('ROLE_PARENT')]
-    public function parentProfile(User $user, MatiereRepository $matiereRepository): Response
-    {
-        $matieres = $matiereRepository->findBy([], ["nom" => "ASC"]);
-        return $this->render('user/parent.html.twig', [
-            'user' => $user,
-            'matieres' => $matieres,
-        ]);
-    }
+        // Création du formulaire
+        $form = $this->createForm(UserType::class, $user);
 
-    #[Route('/user/eleve/{id}', name: 'app_user_eleve')]
-    #[IsGranted('ROLE_ELEVE')]
-    public function eleveProfile(User $user, MatiereRepository $matiereRepository): Response
-    {
-        $matieres = $matiereRepository->findBy([], ["nom" => "ASC"]);
-        return $this->render('user/eleve.html.twig', [
-            'user' => $user,
-            'matieres' => $matieres,
-        ]);
-    }
+        // Gestion de la soumission du formulaire
+        $form->handleRequest($request);
 
-    #[Route('/user/tuteur/{id}', name: 'app_user_tuteur')]
-    #[IsGranted('ROLE_TUTEUR')]
-    public function tuteurProfile(User $user, MatiereRepository $matiereRepository): Response
-    {
-        $matieres = $matiereRepository->findBy([], ["nom" => "ASC"]);
-        return $this->render('user/tuteur.html.twig', [
-            'user' => $user,
+        // Vérification si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarde de la ressource en base de données
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Message de confirmation
+            $this->addFlash('success', 'Utilisateur ajoutée avec succès !');
+
+            // Redirection vers la page de liste des user (à ajuster si nécessaire)
+            return $this->redirectToRoute('app_user');
+        }
+
+        // Rendu de la vue avec le formulaire
+        return $this->render('user/add.html.twig', [
+            'form' => $form->createView(),
             'matieres' => $matieres,
         ]);
     }
+    
 
     #[Route('/user/edit/{id}', name: 'app_user_edit')]
     public function edit(User $user, Request $request, EntityManagerInterface $em, MatiereRepository $matiereRepository): Response
@@ -94,5 +88,13 @@ class UserController extends AbstractController
             'matieres' => $matieres,
         ]);
     }
-
+    #[Route('/user/{id}', name: 'app_show_user')]
+    public function profile(User $user, MatiereRepository $matiereRepository): Response
+    {
+        $matieres = $matiereRepository->findBy([], ["nom" => "ASC"]);
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+            'matieres' => $matieres,
+        ]);
+    }
 }
