fd01d59cca79eebf0beb19d38309680f5aa22a41
AsmaS <saidiasma422@gmail.com>
2024-11-07 16:10:45 +0100

    📦 NEW: Add new folder Model  and new file SearchData for the search bar in the navbar

---
+++ src/Form/SearchType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Form\SearchType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('q', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Rechercher'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => SearchData::class,
+            'method' => 'GET',
+            'csrf_protection' => false,
+        ]);
+    }
+}
