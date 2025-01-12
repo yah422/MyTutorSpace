56e5f79557ec68d4bcbca584aa7d653e990a058a
Asma S <saidiasma422@gmail.com>
2025-01-12 20:08:23 +0100

    📝 Add the SauvegardeProfil part

---
+++ src/Entity/SauvegardeProfil.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SauvegardeProfilRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SauvegardeProfilRepository::class)]
+class SauvegardeProfil
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateSauvegarde = null;
+
+    #[ORM\Column]
+    private array $contenu = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateSauvegarde(): ?\DateTimeInterface
+    {
+        return $this->dateSauvegarde;
+    }
+
+    public function setDateSauvegarde(\DateTimeInterface $dateSauvegarde): static
+    {
+        $this->dateSauvegarde = $dateSauvegarde;
+
+        return $this;
+    }
+
+    public function getContenu(): array
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(array $contenu): static
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+}
