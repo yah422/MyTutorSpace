ef02ab70c3d40e1fb5bfae19e01478be9889f972
Asma S <saidiasma422@gmail.com>
2025-01-26 21:46:48 +0100

    💄 ✨ Add css and js for the ressource and type parts

--- src/Controller/TypeController.php
+++ src/Controller/TypeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TypeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TypeController extends AbstractController
 {
     #[Route('/type', name: 'app_type')]
-    public function index(): Response
+    public function index(TypeRepository $typeRepository): Response
     {
+        $types = $typeRepository->findAll();
+    
         return $this->render('type/index.html.twig', [
-            'controller_name' => 'TypeController',
+            'types' => $types,
         ]);
     }
+    
 }
