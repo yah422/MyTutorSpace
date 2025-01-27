5060093dde3a1e90661b79e8f82694d085212bd9
Asma S <saidiasma422@gmail.com>
2025-01-27 14:20:55 +0100

    💄 🔨 Correct  the css and the JsonResponse for the saveProfil part

--- src/Controller/SauvegardeProfilController.php
+++ src/Controller/SauvegardeProfilController.php
@@ -8,8 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SauvegardeProfilController extends AbstractController
 {
@@ -18,31 +20,36 @@ class SauvegardeProfilController extends AbstractController
         #[CurrentUser] User $user,
         User $tuteur,
         EntityManagerInterface $em
-    ): RedirectResponse {
+    ): JsonResponse {
         // Vérifier si le profil est déjà sauvegardé
         $existingSauvegarde = $em->getRepository(SauvegardeProfil::class)
             ->findOneBy(['user' => $user, 'tuteur' => $tuteur]);
-
+    
         if ($existingSauvegarde) {
-            $this->addFlash('warning', 'Ce profil est déjà sauvegardé.');
-            return $this->redirectToRoute('app_list_saved_profiles');
+            return new JsonResponse([
+                'success' => false,
+                'message' => 'Ce profil est déjà sauvegardé.'
+            ]);
         }
-
+    
         // Créer une nouvelle sauvegarde
         $sauvegarde = new SauvegardeProfil();
         $sauvegarde->setDateSauvegarde(new \DateTime());
         $sauvegarde->setUser($user);
         $sauvegarde->setTuteur($tuteur);
-
+    
         $em->persist($sauvegarde);
         $em->flush();
-
-        $this->addFlash('success', 'Votre profil a été sauvegardé.');
-        return $this->redirectToRoute('app_list_saved_profiles');
-
+    
+        return new JsonResponse([
+            'success' => true,
+            'message' => 'Votre profil a été sauvegardé.'
+        ]);
     }
+    
 
     #[Route('/liste-sauvegardes', name: 'app_list_saved_profiles')]
+    #[IsGranted('ROLE_USER')]
     public function listSavedProfiles(#[CurrentUser] User $user): Response
     {
         $sauvegardes = $user->getSauvegardeProfils();
@@ -50,7 +57,7 @@ class SauvegardeProfilController extends AbstractController
         $tuteur = null;
 
         if (in_array('ROLE_TUTEUR', $user->getRoles(), true)) {
-            $tuteur = $user; // L'utilisateur est lui-même le tuteur
+            $tuteur = $user;
         }
 
         return $this->render('sauvegarde_profil/index.html.twig', [
